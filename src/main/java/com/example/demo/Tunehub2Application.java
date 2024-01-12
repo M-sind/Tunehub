b829a1ffad8ec5e3850ffa39ce7bd3d0936ba82b
Sindhu881 <140969252+Sindhu881@users.noreply.github.com>
2024-01-12 21:23:08 +0530

    lst version of Tunehub

---
+++ src/main/java/com/example/demo/Tunehub2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tunehub2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tunehub2Application.class, args);
+	}
+
+}
